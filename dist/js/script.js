874faddd61cb7acdb47f02ec3b46a6a8bf6f2fd0
ipie <ipiewithapple@gmail.com>
2019-07-05 23:22:08 +0300

    contacts page + consulting page titles' bg

--- dist/js/script.js
+++ dist/js/script.js
@@ -10501,6 +10501,16 @@ return jQuery;
     $(this).data('id', i);
   });
   $consTitle.on('click', function () {
+    $consTitle.not($(this)).attr('class', '');
+
+    if ($(this).data('id') == 0) {
+      $(this).toggleClass('consulting-item--active-1');
+    } else if ($(this).data('id') == 1) {
+      $(this).toggleClass('consulting-item--active-2');
+    } else {
+      $(this).toggleClass('consulting-item--active-3');
+    }
+
     $consDesc.not($(this).next()).slideUp('slow');
     $(this).css('background', '');
     $(this).siblings().slideToggle('slow');
